659ca3ab3d0900254c1ac893dd44209dc8d8c837
jagilley <37783831+jagilley@users.noreply.github.com>
2019-02-23 15:26:49 -0600

    disparity

--- altSolution.py
+++ altSolution.py
@@ -3,7 +3,7 @@ import cv2
 left = cv2.VideoCapture(0)
 right = cv2.VideoCapture(1)
 
-while(True):
+while True:
     if not (left.grab() and right.grab()):
         print("No more frames")
         break
@@ -11,10 +11,13 @@ while(True):
     _, leftFrame = left.retrieve()
     _, rightFrame = right.retrieve()
 
-    cv2.imshow('left', leftFrame)
-    cv2.imshow('right', rightFrame)
+    #cv2.imshow('left', leftFrame)
+    #cv2.imshow('right', rightFrame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    
+    disparity = stereo.compute(imgL,imgR)
+    cv2.imshow(disparity,'gray')
 
 left.release()
 right.release()
