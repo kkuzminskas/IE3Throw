9c2723d1eedcdb9b00ad1104a865e2580ad69243
jagilley <37783831+jagilley@users.noreply.github.com>
2019-02-28 19:29:01 -0600

    asefguy

--- motor.py
+++ motor.py
@@ -3,16 +3,16 @@ from adafruit_crickit import crickit
  
 print("1 Servo 4 demo!")
 
-
 print("Dual motor demo!")
 motor_1 = crickit.dc_motor_1
 motor_2 = crickit.dc_motor_2
  
 while True:
-    motor_1.throttle = 0.9 # full speed forward
+    motor_1.throttle = 0.1 # full speed forward
     time.sleep(0.5)
     crickit.servo_4.angle = 90     # middle
     time.sleep(1)
+    exit()
     
     motor_1.throttle = 0.9 # full speed forward
     time.sleep(0.5)
